f0c2e2a102967752fe8e306b08c692c79afec95a
DiCode77 <dimazamshaq@icloud.com>
2025-10-27 23:12:06 +0200

    Fixed information about displaying the application version.

--- src/Animatica.hpp
+++ src/Animatica.hpp
@@ -17,6 +17,8 @@
 
 #include "obj.hpp"
 
+constexpr const char *APP_VERSION = "0.0.1";
+
 class Animatica : public wxFrame{
     ModernizeWindow *mod_main_window = nullptr;
     Animate         *animate_gif = nullptr;
